02235146aadea4b5fe11ea7057cfee31972abfc1
Eliel-Silva-dev <eliellnr@hotmail.com>
2024-08-19 20:09:52 -0300

    feat: react mario game estilos + script inicial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,33 @@
-import styles from "./page.module.css";
+'use client';
+
+import { useEffect, useRef } from 'react';
+import styles from './page.module.css';
 
 export default function Home() {
+  // class jump: page_jump__Ry29I
+  const mario = useRef({} as HTMLImageElement);
+  useEffect(() => {
+    const loopMario = setInterval(() => {
+      const marioAltura = getComputedStyle(mario.current);
+      console.log(marioAltura.bottom);
+
+    }, 1000);
+  }, []);
+
   return (
     <main id={styles.main_home}>
-      <h2>Página inicial do projeto</h2>
+      <div className={styles.game_board}>
+        <img
+          id={styles.mario}
+          className={styles.jump}
+          src="img/mario.gif"
+          alt="mario"
+          ref={mario}
+        />
+        <img id={styles.pipe} src="img/pipe.png" alt="tubo" />
+        <img id={styles.clounds} src="img/clouds.png" alt="nuvens" />
+      </div>
+      <div className={styles.parede}></div>
     </main>
   );
 }
