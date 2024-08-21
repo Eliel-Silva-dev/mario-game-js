ec092bcf5233201555ba9a1b11051a867cd86371
Eliel-Silva-dev <eliellnr@hotmail.com>
2024-08-21 19:42:07 -0300

    feat: react mario game page politica privacidade

---
+++ src/app/politicaPriv/page.tsx
@@ -0,0 +1,9 @@
+import style from './style.min.module.css';
+
+const PoliticaPriv = () => {
+  <main className={style.politicaPrev}>
+    <h2>Politicas de privacidade</h2>
+  </main>;
+};
+
+export default PoliticaPriv;
