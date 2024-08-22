920d834b65a72c7eec1a8e87a5771733b05d996c
Eliel-Silva-dev <eliellnr@hotmail.com>
2024-08-22 16:35:10 -0300

    fix: react mario js update terms / condictions

--- src/app/politicaPriv/page.tsx
+++ src/app/politicaPriv/page.tsx
@@ -1,9 +1,11 @@
 import style from './style.min.module.css';
 
 const PoliticaPriv = () => {
-  <main className={style.politicaPrev}>
-    <h2>Politicas de privacidade</h2>
-  </main>;
+  return (
+    <main className={style.politicaPrev}>
+      <h2>Politicas de privacidade</h2>
+    </main>
+  );
 };
 
 export default PoliticaPriv;
