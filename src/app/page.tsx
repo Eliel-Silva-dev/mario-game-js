fffbb5281eca6fda283265a1f6c1165f2aa5b731
Eliel-Silva-dev <eliellnr@hotmail.com>
2024-08-23 10:26:02 -0300

    feat: react mario js func play

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,10 @@ import styles from './page.module.css';
 export default function Home() {
   const mario = useRef({} as HTMLImageElement);
   const pipe = useRef({} as HTMLImageElement);
+  const clouds = useRef({} as HTMLImageElement);
   const gameBoard = useRef({} as HTMLDivElement);
+  const boxPlay = useRef({} as HTMLDivElement);
+  const loopId = useRef<NodeJS.Timeout>();
 
   const jumpArrowup = (e: KeyboardEvent) => {
     if (e.key == 'ArrowUp') {
@@ -14,7 +17,7 @@ export default function Home() {
 
       setTimeout(() => {
         mario.current.classList.remove('page_jump__Ry29I');
-      }, 600);
+      }, 500);
     }
   };
 
@@ -26,35 +29,45 @@ export default function Home() {
     }, 600);
   };
 
-  useEffect(() => {
+  const play = () => {
+    pipe.current.classList.add('page_animaPipe__vfR2_');
+    clouds.current.classList.add('page_animaClouds__GRyvg');
+    boxPlay.current.style.display = 'none';
+    mario.current.style.bottom = '0';
+
     document.addEventListener('keydown', jumpArrowup);
 
     document.addEventListener('click', jumpClickMouse);
+  };
 
-    const gameOver = () => {
-      const marioTop = +getComputedStyle(mario.current).bottom.replace(
-        'px',
-        '',
-      );
-      const pipeLeft = pipe.current.offsetLeft;
+  const gameOver = () => {
+    const marioTop = +getComputedStyle(mario.current).bottom.replace('px', '');
+    const pipeLeft = pipe.current.offsetLeft;
 
-      if (pipeLeft < 120 && pipeLeft > 20 && marioTop < 110) {
-        pipe.current.style.animation = 'none';
-        pipe.current.style.left = `${pipeLeft}px`;
+    if (pipeLeft < 120 && pipeLeft > 20 && marioTop < 110) {
+      document.removeEventListener('keydown', jumpArrowup);
 
-        mario.current.style.bottom = `${marioTop}px`;
+      document.removeEventListener('click', jumpClickMouse);
 
-        document.removeEventListener('keydown', jumpArrowup);
+      clouds.current.classList.remove('page_animaClouds__GRyvg');
+      pipe.current.classList.remove('page_animaPipe__vfR2_');
 
-        document.removeEventListener('click', jumpClickMouse);
+      mario.current.style.bottom = `${marioTop}px`;
+      clouds.current.style.right = '-500px';
+      pipe.current.style.right = '-200px';
 
-        clearInterval(loopMario);
-      }
-    };
+      boxPlay.current.style.display = 'flex';
 
-    const loopMario = setInterval(() => {
+      clearInterval(loopId.current);
+    }
+
+    loopId.current = setInterval(() => {
       gameOver();
-    }, 10);
+    }, 50);
+  };
+
+  useEffect(() => {
+    gameOver();
   }, []);
 
   return (
@@ -62,7 +75,17 @@ export default function Home() {
       <div ref={gameBoard} className={styles.game_board}>
         <img id={styles.mario} src="img/mario.gif" alt="mario" ref={mario} />
         <img ref={pipe} id={styles.pipe} src="img/pipe.png" alt="tubo" />
-        <img id={styles.clounds} src="img/clouds.png" alt="nuvens" />
+        <img
+          ref={clouds}
+          id={styles.clounds}
+          src="img/clouds.png"
+          alt="nuvens"
+        />
+        <div ref={boxPlay} id={styles.play}>
+          <button onClick={play} type="button">
+            Play
+          </button>
+        </div>
       </div>
       <div className={styles.parede}></div>
     </main>
